4eefc97eaad32fc72abe9ae0ed696e0b11bf3b36
nehay <nehay.w@gmail.com>
2022-05-30 19:13:59 +0530

    Created a todo task

--- todoList/server.js
+++ todoList/server.js
@@ -16,6 +16,29 @@ app.set('view engine', 'ejs');
 
 app.use('/static', express.static(path.join(__dirname, 'scripts')));
 
+
+
+//the completed task array with initial placeholders for removed task
+// var complete = ["finish jquery"];
+// app.post("/removetask", function(req, res) {
+// 	console.log("===========hello")
+//      var completeTask = req.body.check;
+// //check for the "typeof" the different completed task, then add into the complete task
+// if (typeof completeTask === "string") {
+//      complete.push(completeTask);
+// //check if the completed task already exist in the task when checked, then remove using the array splice method
+//   task.splice(task.indexOf(completeTask), 1);
+// } else if (typeof completeTask === "object") {
+//     for (var i = 0; i < completeTask.length; i++) {     complete.push(completeTask[i]);
+//     task.splice(task.indexOf(completeTask[i]), 1);
+// }
+// }
+//    res.redirect("/");
+// });
+
+
+
+//the task array with initial placeholders for added task
 var task = [];
 async function main_showall() {
 		const all = await interact.contract.viewAllTask();
@@ -24,21 +47,21 @@ async function main_showall() {
 			{
 				task.push({"name" : Object.values(all[i])[0], "isDone" : Object.values(all[i])[1]});
 			}
+			debugger;
 		}
 	}
 	main_showall()
+// 	debugger;
 
 //post route for adding new task
 app.post('/addtask', function (req, res) {
     var newTask = req.body.newtask;
-    
 	async function main() {
 	    const tx = await interact.contract.add_todo_list(newTask);
 	    await tx.wait();
 	}
-
 	main()
-    task.push(newTask);
+    task.push({'name': newTask, 'isDone' : false});
     res.redirect("/");
 });
 
@@ -60,8 +83,8 @@ app.post('/complete/task', function(req, res){
 	main();
 });
 
-app.get("/", function(req, res) {    
-  res.render("todo", { "task": task, "complete": complete });
+app.get("/", function(req, res) {  
+  res.render("todo", { "task": task});
 });
 
 
